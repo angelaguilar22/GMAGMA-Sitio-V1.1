0652ba0fa2655f9d545a481382def1182dfb3310
angel <angel.aguilar@surosystem.com>
2019-06-25 00:35:19 -0500

    algo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //import of routes of page
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 //components of Index view
 import Header from "./components/header";
