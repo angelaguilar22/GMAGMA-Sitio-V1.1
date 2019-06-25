6377331b8ce8610cd6567d75928e2187a40eba4d
angel <angel.aguilar@surosystem.com>
2019-06-24 23:58:27 -0500

    Updates

---
+++ precache-manifest.c2fb8034b3fb031d9180bb112875ca1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d03bcd7c5fd0b08235c6475b1b0168",
+    "url": "/GMAGMA-Sitio-V1.1/index.html"
+  },
+  {
+    "revision": "02aaf79cc62f3aee957b",
+    "url": "/GMAGMA-Sitio-V1.1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GMAGMA-Sitio-V1.1/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "02aaf79cc62f3aee957b",
+    "url": "/GMAGMA-Sitio-V1.1/static/js/main.4ccb3bca.chunk.js"
+  },
+  {
+    "revision": "cdd85c992735cfde2b64",
+    "url": "/GMAGMA-Sitio-V1.1/static/js/runtime~main.99d69f3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GMAGMA-Sitio-V1.1/static/media/logo.5d5d9eef.svg"
+  }
+]);
